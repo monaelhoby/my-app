4449a82bb7b238bf0a4edc0181de67917b81b75b
Mona Mohamed <42801687+monaelhoby@users.noreply.github.com>
2019-09-08 21:42:02 +0200

    Update list.js

--- src/component/list.js
+++ src/component/list.js
@@ -7,6 +7,7 @@ import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import OutlinedInput from '@material-ui/core/OutlinedInput';
 import FormControl from '@material-ui/core/FormControl';
+import '../App.css';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -68,13 +69,9 @@ RenderSimpleForm = () => {
     const classes = this.props;
      return (
        <div>
-      <span style={{display:"inline-block",width:"28%",marginRight:"2%",background:"#FFF",padding:"8px 5px 10px",borderRadius:"5px"}}>{this.props.details.name} </span>
-      <span  style={{display:"inline-block",width:"28%",marginRight:"2%",background:"#FFF",padding:"8px 5px 10px",borderRadius:"5px"}}>{this.props.details.age}</span>
-         <Button variant="contained" type="submit" className={classes.buttone}
-         style={{background: "#7f2a65",display:"inline-block",width:"18%",color:"#FFF",marginRight:"1%" ,padding:"7px"}}
-         onClick={ () => {this.toggleState()}}>
-            Edit
-          </Button>
+      <span className="span1" >{this.props.details.name} </span>
+      <span  className="span2" >{this.props.details.age}</span>
+      <Button className="buttonAdd" variant="contained" type="submit"  onClick={ () => {this.toggleState()}}>Edit  </Button>
        <Button variant="contained" type="submit" className={classes.button}
         onClick={ () => {this.props.deleteRow(this.props.index)}}>
           Delete
@@ -105,68 +102,49 @@ render (){
     },
   })(TextField);
 
-  // const BootstrapInput = withStyles(theme => ({
-  //   root: {
-  //     'label + &': {
-  //       marginTop: theme.spacing(3),
-  //     },
-  //   },
-  //   input: {
-  //     borderRadius: 4,
-  //     position: 'relative',
-  //     border: '2px solid #FFF',
-  //     padding:"15px",
-  //     top:"-21px",
-  //     '&:focus': {
-  //       borderRadius: 4,
-  //       borderColor: '#80bdff',
-  //     },
-  //   },
-  // }))(InputBase);
-
-        //render update addRow
+  //render update addRow
   const RenderUpdateRow = () => {
 
-          const classes = useStyles();
-            const inputLabel = React.useRef(null);
-            const [labelWidth, setLabelWidth] = React.useState(0);
-            React.useEffect(() => {
-              setLabelWidth(inputLabel.current.offsetWidth);
-            }, []);
+    const classes = useStyles();
+      const inputLabel = React.useRef(null);
+      const [labelWidth, setLabelWidth] = React.useState(0);
+      React.useEffect(() => {
+        setLabelWidth(inputLabel.current.offsetWidth);
+      }, []);
 
-           return (
-          <form  className={classes.container} noValidate autoComplete="off" onSubmit={this.updateRow}>
-           <ValidationTextField
-                   id="outlined-name"
-                   label="Name"
-                   className={classes.textField}
-                   defaultValue={this.props.details.name}
-                   margin="normal"
-                   variant="outlined"
-                   inputRef={(v) => {this.inputname = v} }
-                 />
-                 <FormControl variant="outlined" className={classes.formControl}>
-                  <InputLabel ref={inputLabel} htmlFor="age-customized-select">
-                    Age
-                  </InputLabel>
-                  <Select
-                    defaultValue={this.props.details.age}
-                    onChange={(e) => {this.ageValue = e.target.value }}
-                    input={<OutlinedInput labelWidth={labelWidth} name="age" id="age-customized-select" />}
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
-                  </Select>
-                </FormControl>
-                 <Button variant="contained" type="submit" className={classes.buttone}>
-                    Update
-                  </Button>
-           </form>
-         )
+     return (
+    <form  className={classes.container} noValidate autoComplete="off" onSubmit={this.updateRow}>
+     <ValidationTextField
+             id="outlined-name"
+             label="Name"
+             className={classes.textField}
+             defaultValue={this.props.details.name}
+             margin="normal"
+             variant="outlined"
+             inputRef={(v) => {this.inputname = v} }
+           />
+           <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel ref={inputLabel} htmlFor="age-customized-select">
+              Age
+            </InputLabel>
+            <Select
+              value={this.props.details.age}
+              onChange={(e) => {this.ageValue = e.target.value }}
+              input={<OutlinedInput labelWidth={labelWidth} name="age" id="age-customized-select" />}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+           <Button variant="contained" type="submit" className={classes.buttone}>
+              Update
+            </Button>
+     </form>
+   )
         }
 
    return (
