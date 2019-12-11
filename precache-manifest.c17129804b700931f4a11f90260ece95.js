9198c4cd84e26dc81357150e905cccec177c8a8e
mona mohamed <monaelhoby@yahoo.com>
2019-12-11 22:01:34 +0200

    Updates

---
+++ precache-manifest.c17129804b700931f4a11f90260ece95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f2247809c1f57013e8ca70ecd87a7ec",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9f38978d353860ea6deb",
+    "url": "/my-app/static/css/main.77a86610.chunk.css"
+  },
+  {
+    "revision": "80aa65649bdd9c82da67",
+    "url": "/my-app/static/js/2.5d98ac7f.chunk.js"
+  },
+  {
+    "revision": "9f38978d353860ea6deb",
+    "url": "/my-app/static/js/main.832a7899.chunk.js"
+  },
+  {
+    "revision": "00fa2de64e66be72c556",
+    "url": "/my-app/static/js/runtime~main.122bc8ec.js"
+  }
+]);
